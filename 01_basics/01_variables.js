55779830f1ea6750f8cc05b6b89f80d28a596ed8
Aashu kumar <69886020+CaptainAashu@users.noreply.github.com>
2024-09-07 10:49:02 +0000

    changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1443322
+let accountEmail = "aashu@gmail.com"
+var accountpassword = "12345"
+accountCity = "Gaya"
+
+// accountId = 2
+accountEmail = "aa@aashu.com"
+accountpassword = "67899"
+accountCity = "Bengaluru"
+
+
+// console.log(accountId);
+console.table([accountEmail, accountId, accountpassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+// data types
+
+// number => 2^52
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// Symbol => unique
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null)
+
